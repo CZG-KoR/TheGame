265b70a4e5b553e1efd2e91d39db9fe27d80f49a
Jenced <156088542+Jenced@users.noreply.github.com>
2024-01-11 09:31:34 +0100

    Spielererstellung, builder erstellbar, keine teamid

---
+++ player.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication1;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author guest-ho4q2h
+ */
+public class player {
+    //Name eines Spielers
+    String playername;
+    
+    //Anzahl der Credits zum Bauen und Erstellen von Charakteren, die pro Runde dazukommen
+    int credits;
+    
+    //Credits, die ein Spieler aufgrund der Gebaeude und Gebiete pro Runde dazubekommt
+    int creditsperround;
+    
+    //Farbe des Spielers fuer Animation
+    String colour;
+    
+    //Charaktere und Gebaeude eines Spielers
+    ArrayList<Character> Characters= new ArrayList();
+    ArrayList<Building> Buildings = new ArrayList();
+
+    public player(String playername, String colour) {
+        this.playername = playername;
+        this.colour = colour;
+        
+        //Werte zu Beginn des Spiels muessen noch genauer festgelegt werden
+        credits = 5;
+        creditsperround = 2;
+        
+        //Beginn des Spiels mit Rathaus und Builder
+        Characters.add(new Builder());
+        
+        System.out.println("Position fuer Rathaus muss noch erstellt werden");
+        Buildings.add(new Townhall(0,0));
+    }
+    
+    
+    
+}
