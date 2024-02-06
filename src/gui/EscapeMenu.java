bd4b8c979c393d63c0c095e01eb2b8dbfdf29ce6
guest-biuorw <guest-biuorw@R1>
2024-02-06 14:53:04 +0100

    (ノಠ益ಠ)ノ彡┻━┻ escape menu wird angezeigt, aber nicht wenn layeredpane im focus

--- src/gui/EscapeMenu.java
+++ src/gui/EscapeMenu.java
@@ -6,7 +6,6 @@ package gui;
 
 import java.awt.Dimension;
 import java.awt.Toolkit;
-import javax.swing.JButton;
 import javax.swing.JMenuBar;
 
 /**
@@ -15,24 +14,10 @@ import javax.swing.JMenuBar;
  */
 public class EscapeMenu extends JMenuBar {
 
-    JMenuBar mB;
-
     public EscapeMenu() {
-        mB = new JMenuBar();
-        //mB.setSize(200, 300);
-        mB.setPreferredSize(new Dimension(200, 300));
-        mB.setLocation(Toolkit.getDefaultToolkit().getScreenSize().width / 2 - 100, Toolkit.getDefaultToolkit().getScreenSize().height / 2 - 150);
-        mB.setVisible(false);
-        mB.setEnabled(false);
-    }
-    
-    public JButton escapeButton(){
-        JButton eB = new JButton();
-        eB.setSize(50,50);
-        eB.setLocation(Toolkit.getDefaultToolkit().getScreenSize().width / 2 - 150, Toolkit.getDefaultToolkit().getScreenSize().height / 2 - 200);
-        eB.setVisible(true);
-        eB.setEnabled(true);
-        mB.add(eB);
-        return eB;
+        setSize(new Dimension(200, 300));
+        setLocation(Toolkit.getDefaultToolkit().getScreenSize().width / 2 - 100, Toolkit.getDefaultToolkit().getScreenSize().height / 2 - 150);
+        setVisible(false);
+        setEnabled(false);
     }
 }
