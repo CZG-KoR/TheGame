0c8d5620f02a55a4da7d2e772201a25904c39968
guest-rlwnhj <guest-rlwnhj@R1>
2024-02-06 14:50:28 +0100

    Wenn Bar geschlossen/geöffnet wird, Cameraposition ändern Animationen angefangen

--- src/gui/Tilemap.java
+++ src/gui/Tilemap.java
@@ -1,9 +1,12 @@
 package gui;
 
+import building.Building;
+import java.awt.Color;
 import map.Map;
 
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.Point;
 import java.awt.Toolkit;
 import java.awt.event.MouseEvent;
@@ -13,56 +16,151 @@ import java.awt.event.MouseWheelEvent;
 import java.awt.event.MouseWheelListener;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
+import launcher.Start;
+import map.Feld;
+import map.Player;
+import tools.MiscUtils;
 
-public class Tilemap extends JPanel implements MouseListener, MouseMotionListener, MouseWheelListener {
-    private transient Map m;
-    //! startPoint, camX und camY sind static werden aber durch Methoden verändert
-    private Point startPoint;
-    private int camX = 0;
-    private int camY = 0;
-
-    public Tilemap(int width, int height, Map m) {
+public class Tilemap extends JPanel implements MouseListener, MouseMotionListener, MouseWheelListener{
+    Map m;
+    Bar b;
+    Point startPoint;
+    int camX = 0;
+    int camY = 0;
+    int hoveredX = 0;
+    int hoveredY = 0;
+    
+    int curTime;
+    
+    Animation a;
+    
+    Feld selectedFeld;
+    character.Character selectedCharacter;
+    Building selectedBuilding;
+    
+    
+    /**
+     * @param m
+     */
+    public Tilemap(int width, int height, Map m, Bar b) {
         super();
         this.m = m;
+        this.b = b;
         this.setSize(new Dimension(width, height));
         this.setLocation(0, 0);
         this.setVisible(true);
-        // Testfall
+        //Testfall
         m.setT(3, 3, "desert");
+        
+        camX = -(m.getWidth() * 64) / 4;
+        camY = -(m.getHeight() * 64) / 4;
+        
+        
+        // Testweise Animationen erstellen
+        Image[] imageA = MiscUtils.loadImages("src/gui/res/terrain");
+        a = new Animation(imageA);
+        
 
     }
 
-    // painComponent; erst schwarzer Hintergrund
+    //painComponent; erst schwarzer Hintergrund
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
+        
+        curTime += 1;
 
         for (int i = 0; i < m.getWidth(); i++) {
             for (int j = 0; j < m.getHeight(); j++) {
                 g.drawImage(m.getTerrainPicture(i, j), 64 * i + camX, 64 * j + camY, null);
+                
             }
         }
-        // g.drawImage(Toolkit.getDefaultToolkit().getImage("src/GUI/res/ResourceBar.png"),
-        // 0, 0, null);
-
+        
+        // zeichne markierungen für von maus berührte felder
+        g.setColor(Color.DARK_GRAY);
+        g.drawRect(64 * hoveredX + camX, 64 * hoveredY + camY, 64, 64);
+        
+        if (selectedFeld != null){
+            g.setColor(Color.magenta);
+            g.drawRect(64 * selectedFeld.getxPosition() + camX, 64 * selectedFeld.getyPosition() + camY, 64, 64);
+        }
+        
+        g.drawImage(a.getCurImg(curTime), 1, 1, null);
+        
+        //g.drawImage(Toolkit.getDefaultToolkit().getImage("src/GUI/res/ResourceBar.png"), 0, 0, null);
+        
     }
-
-    // Grenze für scroll
-    private int clamp(int a, int min, int max) {
+    
+    //Grenze für scroll
+    private int clamp(int a, int min, int max){
         if (a > max) {
             return max;
         }
-
-        if (a < min) {
+        
+        if (a < min){
             return min;
         }
-
+        
         return a;
-
+        
     }
+    
+    private int booleanToInt(boolean foo) {
+    int bar = 0;
+    if (foo) {
+        bar = 1;
+    }
+    return bar;
+}
+    
+    private void setSelectedCharacter(){
+        Player[] p = Start.getPlayers();
+        selectedCharacter = null;
+        
+        for (int i = 0; i < p.length; i++) {
+            Player player = p[i];
+            
+            if (player.getCharacter(hoveredX, hoveredY) != null){
+                selectedCharacter = player.getCharacter(hoveredX, hoveredY);
+            }
+        }
+        
+        System.out.println("Selected Character: " + selectedCharacter);
+        
+        
+    }
+    
+    private void setSelectedBuilding(){
+        Player[] p = Start.getPlayers();
+        selectedBuilding = null;
+        
+        for (int i = 0; i < p.length; i++) {
+            Player player = p[i];
+            
+            if (player.getBuilding(hoveredX, hoveredY) != null){
+                selectedBuilding = player.getBuilding(hoveredX, hoveredY);
+            }
+        }
+        
+        System.out.println("Selected Building: " + selectedBuilding);
+        
+        
+    }
+    
 
     @Override
     public void mouseClicked(MouseEvent e) {
+        // angeklicktes Feld auswählen
+        selectedFeld = m.getFeld(hoveredX, hoveredY);
+        
+        // angeklickte Character finden
+        setSelectedCharacter();
+        
+        // angeklickte Gebäude finden
+        setSelectedBuilding();
+        
+        
     }
 
     @Override
@@ -85,33 +183,51 @@ public class Tilemap extends JPanel implements MouseListener, MouseMotionListene
 
     @Override
     public void mouseDragged(MouseEvent e) {
-        if (startPoint == null)
-            return;
-
+        if (startPoint == null) return;
+        
         Point location = SwingUtilities.convertPoint(this, e.getPoint(), this.getParent());
-
-        if (this.getRootPane().getBounds().contains(location)) {
+        
+        if (this.getRootPane().getBounds().contains(location)){
             Point newLocation = this.getLocation();
             newLocation.translate(location.x - startPoint.x, location.y - startPoint.y);
 
             camX += newLocation.x;
             camY += newLocation.y;
-
+            
             // Grenze von unten rechts und oben links festgelegt
-            camX = clamp(camX, (m.getWidth() * 64 - Toolkit.getDefaultToolkit().getScreenSize().width) * -1, 0);
-            camY = clamp(camY, (m.getHeight() * 64 - Toolkit.getDefaultToolkit().getScreenSize().height) * -1, 0);
-
+            limitCamera();
+           
+            
             startPoint = location;
-        } else
-            startPoint = null;
+        } else startPoint = null;
+    }
+    
+    public void limitCamera(){
+         int minX = (m.getWidth()*64 - Toolkit.getDefaultToolkit().getScreenSize().width)*-1;
+            camX = clamp(camX, minX, 0);
+            
+            int minY = (m.getHeight()*64 - Toolkit.getDefaultToolkit().getScreenSize().height)*-1;
+            camY = clamp(camY,minY - (b.getHeight() * booleanToInt(b.isVisible())), 0);
+    }
+    
+    public void barOpened(){
+        int minY = (m.getHeight()*64 - Toolkit.getDefaultToolkit().getScreenSize().height)*-1;
+        
+        if (camY < minY + b.getHeight()){
+            camY -= b.getHeight();
+        }
+        
     }
 
     @Override
     public void mouseMoved(MouseEvent e) {
+        Point clicked = SwingUtilities.convertPoint(this.getParent(), e.getPoint(), this.getParent());
+        hoveredX = (clicked.x - camX) / 64;
+        hoveredY = (clicked.y - camY ) / 64;
     }
 
     @Override
     public void mouseWheelMoved(MouseWheelEvent e) {
-        // tileSize -= e.getUnitsToScroll();
+        //tileSize -= e.getUnitsToScroll();
     }
 }
