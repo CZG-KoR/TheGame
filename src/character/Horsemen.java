6ec75e2d0d8099fd49423efe3e76eb1686f74dc6
guest-cf5rty <guest-cf5rty@R1>
2024-02-20 14:48:09 +0100

    verbotene terrains angefangen

--- src/character/Horsemen.java
+++ src/character/Horsemen.java
@@ -31,4 +31,9 @@ public class Horsemen extends Fighter{
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
     
+    @Override
+    public void blockedterrains() {
+        this.getBlockedterrains().add("water");
+    }
+    
 }
